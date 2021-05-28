0c6aabd3fd7cab1a8da62b04ee20c5d0ab921028
Shaza_Asidah <shathaasidah@gmail.com>
2021-05-28 23:45:14 +0300

    add_template

---
+++ resources/views/test.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Shaza Asidah</title>
+</head>
+<body>
+    <h1>This is test View</h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', 'PagesController@index');
+
+Route::get('/shaza', 'ShazaController@index');
+
+
+
